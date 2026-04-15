b75622520f3575f7b74ded0f4d9550291626281a
mikamikamikee <288878@student.pwr.edu.pl>
2026-04-15 15:53:01 +0200

    Added data loading modules

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,33 +1,7 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "c6849484-338b-4893-ad40-e01e43f41770",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidf_vectorize(train_texts, test_texts):
+    vectorizer = TfidfVectorizer(max_features=5000)
+    X_train = vectorizer.fit_transform(train_texts)
+    X_test = vectorizer.transform(test_texts)
+    return X_train, X_test, vectorizer
